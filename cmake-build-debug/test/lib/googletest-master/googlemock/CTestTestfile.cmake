c36db55ffe7b811419b0c43b137403ea5b508d90
federico magini <federico.magini@stud.unifi.it>
2019-09-17 17:33:46 +0200

    Tolta directory di build

--- cmake-build-debug/test/lib/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sode/Documenti/CLionWorkspace/GameProject/test/lib/googletest-master/googlemock
-# Build directory: /home/sode/Documenti/CLionWorkspace/GameProject/cmake-build-debug/test/lib/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
